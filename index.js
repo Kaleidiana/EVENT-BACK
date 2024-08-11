2a966d042844e65087d916b296376d3d6f3e2c5d
Kaleidiana <kaleidiana63@gmail.com>
2024-08-11 22:30:00 +0300

    Made changes to the index.js file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); // Import the cors package
 const userRoute = require('./Routes/userRoute');
-const authRoute = require('./Routes/authRoute'); 
-const eventRoute = require('./Routes/eventRoute');  // Ensure the path is correct
+const authRoute = require('./Routes/authRoute');
+const eventRoute = require('./Routes/eventRoute');
 
 require('dotenv').config(); // Load environment variables
 
@@ -12,7 +13,9 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const app = express();
 
+app.use(cors()); // Enable CORS
 app.use(express.json());
+
 app.use('/api', userRoute); // Prefix routes with /api
 app.use('/api', authRoute);
 app.use('/api', eventRoute);
